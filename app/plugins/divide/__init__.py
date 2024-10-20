90841f6191ba9656b3245f053b816e2f27618265
root <root@AnthonyASUS>
2024-10-19 22:31:51 -0400

    Added logging, added environment, added while loops for input to repeat if return results are not true, done with homework6

--- app/plugins/divide/__init__.py
+++ app/plugins/divide/__init__.py
@@ -1,24 +1,29 @@
 from app.commands import Command
 from calculator.arithmetic_operations import divide
 import os
+import logging
 class DivideCommand(Command):
     def execute(self):
         '''Using os.system(cls,clear) to give the user a fresh screen'''
         os.system('cls') # Windows
         os.system('clear') #Linux/MacOS
+        logging.info("Division operation started.")
+        
         # User enters two number, (automatically float type)
-        try:
-            a = float(input("Enter the first number: "))
-            b = float(input("Enter the second number: "))
-            
-            # Check for division by zero
-            if b == 0:
-                print("Error: Division by zero is not allowed.")
-                return
-            
-            result = divide(a, b)
-            print(f"The result of {a} / {b} = {result}")
+        while True:
+            try:
+                a = float(input("Enter the first number: "))
+                b = float(input("Enter the second number: "))
+                
+                # Check for division by zero
+                if b == 0:
+                    logging.critical("Error: Division by zero is not allowed.")
+                    continue
+                
+                result = divide(a, b)
+                logging.info(f"The result of {a} / {b} = {result}")
+                break
 
-        # Error if user does not enter float type
-        except ValueError:
-            print("Please enter valid numbers.")
+            # Error if user does not enter float type
+            except ValueError:
+                logging.warning("Please enter valid numbers.")
--- app/plugins/subtract/__init__.py
+++ app/plugins/subtract/__init__.py
@@ -1,20 +1,25 @@
 from app.commands import Command
 from calculator.arithmetic_operations import subtract
 import os
+import logging
 class SubtractCommand(Command):
     def execute(self):
         '''Using os.system(cls,clear) to give the user a fresh screen'''
         os.system('cls') # Windows
         os.system('clear') #Linux/MacOS
+        logging.info("Subtraction operation started.")
+        
         
         # User enters two number, (automatically float type)
-        try:
-            a = float(input("Enter the first number: "))
-            b = float(input("Enter the second number: "))
-            result = subtract(a, b)
-    
-            print(f"The result of {a} - {b} = {result}")
+        while True:
+            try:
+                a = float(input("Enter the first number: "))
+                b = float(input("Enter the second number: "))
+                result = subtract(a, b)
         
-        # Error if user does not enter float type
-        except ValueError:
-            print("Please enter valid numbers.")
+                logging.info(f"The result of {a} - {b} = {result}")
+                break
+            
+            # Error if user does not enter float type
+            except ValueError:
+                logging.warning("Please enter valid numbers.")
